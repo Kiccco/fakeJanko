34a9caf9e0d82ea6f20dfd64afc326ca4a771f12
Kiccco <kristjan.kumlanc@gmail.com>
2020-10-10 13:00:23 +0200

    Mysql to true

--- src/main/java/me/kristjan/fakejanko/Settings.java
+++ src/main/java/me/kristjan/fakejanko/Settings.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 public class Settings {
 
 
-    public static String TOKEN = "SVOJ TOKEN";
+    public static String TOKEN = "NzU4MzEwMzQ0MDk0Nzc3Mzg1.X2tFig.CkRVq-5hA5WZRtwlEtQhfym1Y0o";
     public static boolean MYSQL = true;
     public static String HOSTNAME = "localhost";
     public static String PORT = "3306";
